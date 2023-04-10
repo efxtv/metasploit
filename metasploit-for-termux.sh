95b018730111ca2c9c3a44cac1c3f073195a2f0e
Tech EFX Tv <59799893+efxtv@users.noreply.github.com>
2023-04-11 00:55:21 +0530

    Update metasploit-for-termux.sh

--- metasploit-for-termux.sh
+++ metasploit-for-termux.sh
@@ -1,18 +1,1 @@
-#!/data/data/com.termux/files/usr/bin/bash
-pkg install curl wget
-pkg update -y && pkg upgrade -y
 
-wget https://github.com/Hax4us/TermuxBlack/raw/master/install.sh
-chmod +x install.sh
-bash install.sh -i
-pkg update -y && pkg upgrade -y
-echo
-clear
-echo
-echo "Installing Metasploit..."
-sleep 5
-apt update && apt upgrade
-apt install curl wget python ruby
-curl -LO https://raw.githubusercontent.com/Hax4us/Metasploit_termux/master/metasploit.sh
-chmod +x metasploit.sh
-./metasploit.sh
